1f521e1ce2000f5f5436a74a59e39fce0a12abf4
azarateo <azarateo@gmail.com>
2022-11-28 18:31:47 +0800

    An example of use of export and required in NodeJS

---
+++ export_require/app.js
@@ -0,0 +1,11 @@
+let fs = require('fs');
+
+const PI = 3.1416
+function sum(a,b){
+    return a+b
+}
+const sub = (a,b)=>a-b
+
+module.exports.PI = PI
+module.exports.sum = sum
+module.exports.sub = sub
---
+++ export_require/app2.js
@@ -0,0 +1,3 @@
+let mylib = require('./app.js')
+
+console.log(mylib.sub(mylib.sum(3,4),mylib.PI))
